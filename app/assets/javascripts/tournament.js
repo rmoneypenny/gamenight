760ad5994ad308a8d61fb4552963f90f1fc9f829
Randy <r_moneypenny@hotmail.com>
2017-03-21 01:07:48 -0400

    Working on Game Weight

--- app/assets/javascripts/tournament.js
+++ app/assets/javascripts/tournament.js
@@ -29,26 +29,33 @@ $(document).on("turbolinks:load", function(){
         var g = games.split('!*!');
         var gameid = ($(this).find("#gameIDs").val());
         var gid = gameid.split(" ");
-        var buttons = "<button class=\"btn btn-default btn-xs subtract-weight\"> - </button><button class=\"btn btn-default btn-xs add-weight\" style=\"display:none\"> + </button>";
+        var buttons = "<button class=\"btn btn-primary subtract-weight\" type=\"button\">-</button>"
+                    + "<button class=\"btn btn-secondary add-weight\" type=\"button\" style=\"display:none\">+</button>";
         list.text("");
         for(var i=0; i<g.length-1; i++){
-            list.append("<li id=" + gid[i] + ">" + g[i] + "<textarea id=\"weight\" rows=\"1\" cols=\"10\">" + 0 + "</textarea>" + "<button class=\"btn btn-default btn-xs subtract-weight\"> - </button><button class=\"btn btn-default btn-xs add-weight\" style=\"display:none\"> + </button>" + "</li>");
-            //list.append(gid[i] + g[i] + "weight" + buttons);
+            list.append("<li id=" + gid[i] +">"
+                        + "<div class=\"input-group input-group-sm\">"
+                        + "<span class=\"input-group-addon\">" + g[i] + "</span>"
+                        + "<input type=\"text\" class=\"form-control\" id=\"weight\" value=\"1\" readonly=\"readonly\">"
+                        + "<span class=\"input-group-btn\">"
+                        + buttons
+                        + "</span>"
+                        + "</div>"
+                        + "</li>");
               
         };
         var info = $(this).find("#info").val();
         $("#joinedGameDate").text(info);
     });
-
-
-
+       
 
     $("#buttonNumber").on("click", ".subtract-weight",function(){
+        alert($(this).closest("li").attr("id"));
         var weight = parseInt($("#weight").val());
         if (weight > 0){
             $("#weight").val(weight - 1);
-            $(".subtract-weight").hide();
             $(".add-weight").show();
+            $(this).closest("ul").find(".subtract-weight").hide();
         };
     });
 
@@ -57,6 +64,23 @@ $(document).on("turbolinks:load", function(){
     //     $("#weight").val(weight + 1);
     //     $(".add-weight").hide();
     // });
+
+  //list.append(gid[i] + g[i] + "weight" + buttons);
+          
+ // = "<div class=\"input-group input-group-sm\">"
+ //                    + "<span class=\"input-group-addon\">GameName</span>"
+ //                    + "<input type=\"text\" class=\"form-control\" placeholder=\"Weight\">"
+ //                    + "<span class=\"input-group-btn\">"
+ //                    + "<button class=\"btn btn-primary\" type=\"button\">-</button>"
+ //                    + "<button class=\"btn btn-secondary\" type=\"button\">+</button>"
+ //                    + "</span>"
+ //                    + "</div>";
+
+
+// list.append("<li id=" + gid[i] + ">" + g[i] 
+//                         + "<textarea id=\"weight\" rows=\"1\" cols=\"10\">" + 1 + "</textarea>" + buttons);
+     
+
 
 });   
 
